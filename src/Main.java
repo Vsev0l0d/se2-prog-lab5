e41afdceb9c45a40dfab2b456b0ce28e84461222
Vsevolod <vsevolod628@gmail.com>
2020-02-06 12:44:29 +0300

    условие закрытия проги (команда exit)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        String command;
+        do {
+            command = sc.nextLine();
+        } while(!command.equals("exit"));
     }
 }
