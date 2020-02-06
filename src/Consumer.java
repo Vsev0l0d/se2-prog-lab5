4a541581b52e8ef8e3023877996b139e25ac1daa
Vsevolod <vsevolod628@gmail.com>
2020-02-06 19:25:09 +0300

    добавленна карта команд (все что связано с Consumer пока что хрень)

---
+++ src/Consumer.java
@@ -0,0 +1,7 @@
+public class Consumer { // просто хрень пока что
+    private int r;
+
+    public Consumer(int r) {
+        this.r = r;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.TreeSet;
 
@@ -7,8 +8,26 @@ public class Main {
         String command;
         TreeSet<Flat> flats = new TreeSet<Flat>();
 
-        do {
+        Consumer r = new Consumer(1); // все что связано с Consumer пока что хрень
+        HashMap<String, Consumer> commandMap = new HashMap<String, Consumer>();
+        commandMap.put("help", r);
+        commandMap.put("info", r);
+        commandMap.put("show", r);
+        commandMap.put("add", r);
+        commandMap.put("update", r);
+        commandMap.put("remove_by_id", r);
+        commandMap.put("clear", r);
+        commandMap.put("save", r);
+        commandMap.put("execute_script", r);
+//        commandMap.put("exit", r);
+        commandMap.put("add_if_min", r);
+        commandMap.put("remove_greater", r);
+        commandMap.put("remove_lower", r);
+        commandMap.put("group_counting_by_creation_date", r);
+        commandMap.put("filter_contains_name", r);
+        commandMap.put("print_field_ascending_number_of_rooms", r);
 
+        do {
             command = sc.nextLine();
         } while(!command.equals("exit"));
     }
