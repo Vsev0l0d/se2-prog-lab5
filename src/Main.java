b12b9fd009e5b96ccdf86b9c950e098656021323
Vsevolod <vsevolod628@gmail.com>
2020-02-06 18:46:09 +0300

    добавленна коллекция

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String command;
+        TreeSet<Flat> flats = new TreeSet<Flat>();
+
         do {
+
             command = sc.nextLine();
         } while(!command.equals("exit"));
     }
