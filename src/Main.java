1b1a0d135c0d7b35de44fa6de9be60cd10513ccc
Vsevolod <vsevolod628@gmail.com>
2020-02-06 22:47:36 +0300

    сделал несколько команд

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.TreeSet;
 
@@ -11,7 +13,16 @@ public class Main {
             command = sc.nextLine();
             switch (command){
                 case ("help"):
-
+                    try(FileReader reader = new FileReader(System.getProperty("user.dir")+"\\src\\HelpAboutTeams.txt"))
+                    {
+                        int c;
+                        while((c=reader.read())!=-1){
+                            System.out.print((char)c);
+                        }
+                    }
+                    catch(IOException ex){
+                        System.out.println(ex.getMessage());
+                    }
                     break;
                 case ("info"):
 
@@ -37,6 +48,8 @@ public class Main {
                 case ("execute_script"):
 
                     break;
+                case ("exit"):
+                    break;
                 case ("add_if_min"):
 
                     break;
@@ -56,7 +69,7 @@ public class Main {
 
                     break;
                 default:
-
+                    System.out.println("не знаю такую команду, воспользуйтесь командой 'help'");
                     break;
             }
         } while(!command.equals("exit"));
