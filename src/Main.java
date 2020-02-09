a81cdd3819ac24b0fb658707706bd51d8f9ed79b
Vsevolod <vsevolod628@gmail.com>
2020-02-09 17:30:48 +0300

    доступ к массиву текущих слов для остальных классов

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 import java.util.TreeSet;
 
 public class Main {
+    private static String inputLine;
+    private static String[] inputWord;
+
+    public static String[] getInputWord() {
+        return inputWord;
+    }
+
     public static void main(String[] args){
         TreeSet<Flat> flats = new TreeSet<Flat>();
         BankTeams bankTeams = new BankTeams();
         Scanner sc = new Scanner(System.in);
-        String inputLine;
-        String[] inputWord;
 
         while (sc.hasNext()){
             inputLine = sc.nextLine();
