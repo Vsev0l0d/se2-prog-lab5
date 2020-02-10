bf9c50ff6c6c3ceddf096b19a3222ce938c9f188
Vsevolod <vsevolod628@gmail.com>
2020-02-11 00:20:56 +0300

    пустая строка в консоли

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,16 @@ public class Main {
 
         while (sc.hasNext()){
             String[] arguments = sc.nextLine().split("\\s");
+            if (arguments.length == 0) continue;
             String command = arguments[0];
             arguments = Arrays.copyOfRange(arguments, 1, arguments.length);
 
             if (bankTeams.commandMap.get(command) != null){
                 bankTeams.commandMap.get(command).execute(collection, arguments);
             } else {
-                if (!command.equals("")) {
-                    System.out.println("я не знаю такой команды, воспользуйтесь командой help");
-                }
+                System.out.println("я не знаю такой команды, воспользуйтесь командой help");
             }
+
         }
     }
 }
